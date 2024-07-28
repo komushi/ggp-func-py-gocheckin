eac13ea06ed3c1ce26eb3c67805dd1dd73de011a
Lei Xu <komushi@gmail.com>
2024-07-29 01:16:08 +0900

    set_recording_time

--- py_handler.py
+++ py_handler.py
@@ -267,7 +267,7 @@ def start_http_server():
                     if event['cameraItem']['localIp'] in thread_gstreamers and thread_gstreamers[event['cameraItem']['localIp']] is not None:
                         # record 
                         thread_gstreamers[event['cameraItem']['localIp']].start_recording()
-                        set_recording_time()
+                        set_recording_time(thread_gstreamers[event['cameraItem']['localIp']], 10)
 
                         self.send_response(200)
                         self.send_header('Content-type', 'application/json')
