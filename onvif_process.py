db1e8a5ce72b69489ca4b3f652e8be0e0e1fdc7e
Lei Xu <komushi@gmail.com>
2024-09-04 19:23:19 +0900

    debug

--- onvif_process.py
+++ onvif_process.py
@@ -14,7 +14,7 @@ import xml.etree.ElementTree as ET
 
 # Setup logging to stdout
 logger = logging.getLogger(__name__)
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 def extract_notification(raw_payload):
     # Parse the XML content
