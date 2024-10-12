7144ef61ce98e9812ecd8d81cb277e03bbefe079
Lei Xu <komushi@gmail.com>
2024-10-12 21:36:00 +0900

    framerate testing temp

--- gstreamer_threading.py
+++ gstreamer_threading.py
@@ -96,7 +96,7 @@ class StreamCapture(threading.Thread):
             pipeline_str_decode = f"""
                 appsrc name=m_appsrc emit-signals=true is-live=true format=time
                 ! queue ! h265parse ! queue ! avdec_h265 name=m_avdec max-threads=2 output-corrupt=false
-                ! queue ! videoconvert ! videorate drop-only=true ! video/x-raw,format=BGR,framerate=5/1
+                ! queue ! videoconvert ! videorate ! video/x-raw,format=BGR,framerate=5/1
                 ! queue ! appsink name=m_appsink"""
             # pipeline_str_decode = f"""appsrc name=m_appsrc emit-signals=true is-live=true format=time
             #     ! queue ! h265parse ! queue ! avdec_h265 name=m_avdec max-threads=2 output-corrupt=false
