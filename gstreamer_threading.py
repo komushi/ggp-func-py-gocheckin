edb4845985ac7b72183542d008ce3ece4162f428
Lei Xu <komushi@gmail.com>
2024-10-12 18:43:37 +0900

    framerate testing

--- gstreamer_threading.py
+++ gstreamer_threading.py
@@ -96,11 +96,11 @@ class StreamCapture(threading.Thread):
             pipeline_str_decode = f"""
                 appsrc name=m_appsrc emit-signals=true is-live=true format=time
                 ! queue ! h265parse ! queue ! avdec_h265 name=m_avdec max-threads=2 output-corrupt=false
-                ! queue ! videoconvert ! videorate drop-only=true ! video/x-raw,format=BGR,framerate={round(int(self.framerate) * float(os.environ['DETECTING_RATE_PERCENT']))}/1
+                ! queue ! videoconvert ! videorate drop-only=true max-rate={round(int(self.framerate) * float(os.environ['DETECTING_RATE_PERCENT']))} ! video/x-raw,format=BGR
                 ! queue ! appsink name=m_appsink"""
             # pipeline_str_decode = f"""appsrc name=m_appsrc emit-signals=true is-live=true format=time
             #     ! queue ! h265parse ! queue ! avdec_h265 name=m_avdec max-threads=2 output-corrupt=false
-            #     ! queue ! videoconvert ! videorate drop-only=true ! video/x-raw,format=BGR,framerate=10/2
+            #     ! queue ! videoconvert ! videorate drop-only=true max-rate={round(int(self.framerate) * float(os.environ['DETECTING_RATE_PERCENT']))} ! video/x-raw,format=BGR,framerate={round(int(self.framerate) * float(os.environ['DETECTING_RATE_PERCENT']))}/1
             #     ! queue ! appsink name=m_appsink"""
 
         # Create the empty pipeline
