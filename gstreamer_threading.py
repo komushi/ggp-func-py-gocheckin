e78c4a48bb2aee265b0ec4998f12b158ba157006
Lei Xu <komushi@gmail.com>
2024-10-12 17:16:34 +0900

    framerate testing

--- gstreamer_threading.py
+++ gstreamer_threading.py
@@ -101,7 +101,7 @@ class StreamCapture(threading.Thread):
             pipeline_str_decode = f"""
                 appsrc name=m_appsrc emit-signals=true is-live=true format=time
                 ! queue ! h265parse ! queue ! avdec_h265 name=m_avdec max-threads=2 output-corrupt=false
-                ! queue ! videoconvert
+                ! queue ! videoconvert ! video/x-raw,format=BGR
                 ! queue ! appsink name=m_appsink"""
 
         # Create the empty pipeline
