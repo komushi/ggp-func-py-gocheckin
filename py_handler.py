8bf5f88cbc3ac69bdd4dd4c8a88de8113e77bf1a
Lei Xu <komushi@gmail.com>
2025-02-05 07:13:08 +0900

    stop_feeding WIP

--- py_handler.py
+++ py_handler.py
@@ -1217,7 +1217,7 @@ def subscribe_onvif():
             logger.error(f"subscribe_onvif, Exception during running, Error: {e}")
             pass
     
-    timer = threading.Timer(120, subscribe_onvif)
+    timer = threading.Timer(1800, subscribe_onvif)
     timer.name = "Thread-SubscribeOnvif-Timer"
     timer.start()
 
