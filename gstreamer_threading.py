9e0055148f60cf65619b5428576ac425866a72ed
Lei Xu <komushi@gmail.com>
2025-02-09 22:33:24 +0900

    current time

--- gstreamer_threading.py
+++ gstreamer_threading.py
@@ -247,7 +247,7 @@ class StreamCapture(threading.Thread):
                     caps_string
                 )
 
-            caps_string += f", x-custom-meta=(fraction)${str(current_time)}"
+            caps_string += f", x-custom-meta=(string)${str(current_time)}"
 
             new_caps = Gst.Caps.from_string(caps_string)
             
