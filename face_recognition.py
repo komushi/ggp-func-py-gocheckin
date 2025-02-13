d34d97b8cbb2fc3a1916ee245d42ca56ed5d8989
Lei Xu <komushi@gmail.com>
2025-02-13 12:39:34 +0900

    DETECT_RUNNING_TIME/10

--- face_recognition.py
+++ face_recognition.py
@@ -63,8 +63,9 @@ class FaceRecognition(threading.Thread):
                                 self.inference_begins_at = time.time()
 
                                 faces = []
+                                logger.info(f"{cam_info['cam_ip']} inference_begins_at: {datetime.fromtimestamp(time.time(), timezone(timedelta(hours=9))).strftime('%H:%M:%S.%f')[:-3]} frame's age of {self.inference_begins_at - float(cam_info['frame_time'])} in seconds.")
                                 if self.inference_begins_at - float(cam_info['frame_time']) > float(os.environ['DETECT_RUNNING_TIME']) / 5:
-                                    logger.error(f"{cam_info['cam_ip']} inference_begins_at: {datetime.fromtimestamp(time.time(), timezone(timedelta(hours=9))).strftime('%H:%M:%S.%f')[:-3]} frame's age of {self.inference_begins_at - float(cam_info['frame_time'])} in seconds.")
+                                    logger.debug(f"{cam_info['cam_ip']} inference_begins_at: {datetime.fromtimestamp(time.time(), timezone(timedelta(hours=9))).strftime('%H:%M:%S.%f')[:-3]} frame's age of {self.inference_begins_at - float(cam_info['frame_time'])} in seconds.")
                                 else:
                                     # logger.info(f"{cam_info['cam_ip']} frame's age of {self.inference_begins_at - float(cam_info['frame_time'])} in seconds.")
                                     logger.debug(f"{cam_info['cam_ip']} inference_begins_at: {datetime.fromtimestamp(time.time(), timezone(timedelta(hours=9))).strftime('%H:%M:%S.%f')[:-3]} frame's age of {self.inference_begins_at - float(cam_info['frame_time'])} in seconds.")
