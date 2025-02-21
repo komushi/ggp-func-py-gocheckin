462c5f5f86e98f14d157453393c0713272328b54
Lei Xu <komushi@gmail.com>
2025-02-21 22:10:09 +0900

    fetch_camera_items

--- py_handler.py
+++ py_handler.py
@@ -915,9 +915,9 @@ def start_scheduler_threads():
     time.sleep(2)
     
     # Start the FetchCamera thread
-    # fetch_camera_thread = threading.Thread(target=fetch_camera_items, name="Thread-FetchCamera")
-    # fetch_camera_thread.start()
-    # logger.info("Fetch camera thread started")
+    fetch_camera_thread = threading.Thread(target=fetch_camera_items, name="Thread-FetchCamera")
+    fetch_camera_thread.start()
+    logger.info("Fetch camera thread started")
 
     # time.sleep(2)
 
